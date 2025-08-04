13ea386537351e0e07f55c08ed1e0e5cf98c6ad4
Pratibha <116498537+Kawaii-sama@users.noreply.github.com>
2025-08-04 22:20:54 +0530

    Add files via upload

---
+++ 4->1.cpp
@@ -0,0 +1,19 @@
+/*
+print  inverted number right angle 2
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printSquare (int num) {
+    for (int i = num; i > 0; i--) {
+        cout << "\n";
+        for (int j = 1; j <= i; j++) {
+            cout << i; }
+    } }
+
+int main () {
+    int input1; cin >> input1;
+    printSquare(input1);
+    return 0;
+}
