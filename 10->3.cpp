ed4bd35b2fa855b9227c7d602a7103b46f8fe4e2
Pratibha <116498537+Kawaii-sama@users.noreply.github.com>
2025-08-23 21:11:50 +0530

    Add files via upload

---
+++ 10->3.cpp
@@ -0,0 +1,29 @@
+// starSquare
+#include <bits/stdc++.h>
+using namespace std;
+
+void starGapSquare (int n) {
+    for (int i = 1; i <= n; i++) {
+           
+           
+           if (i == 1 || i ==n) {
+               for (int j = 0; j < n; j++) {
+                   cout << " * "; }
+            } 
+
+            else {
+             cout << " * ";
+             for (int k = 0 ; k < n-2; k++) {
+                cout << "   "; }
+             cout << " * "; }
+             
+             
+    cout << "\n";
+    }
+}
+
+int main () {
+    int n; cin >> n;
+    starGapSquare(n);
+    return 0;
+}
