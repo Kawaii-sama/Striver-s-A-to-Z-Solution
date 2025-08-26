27a0798c25d3cd16e66f81c0c1555ee3c7ff0918
Pratibha <116498537+Kawaii-sama@users.noreply.github.com>
2025-08-26 23:27:53 +0530

    Update 12->1.cpp

--- 12->1.cpp
+++ 12->1.cpp
@@ -1,8 +1,6 @@
-/count digits in a number (striver)
+//count digits in a number (striver)
 #include<bits/stdc++.h>
 using namespace std;
-
-
 class Solution {
 public:
     int countDigit (int n) {
@@ -16,4 +14,30 @@ int main () {
     int n; cin >> n;
     cout << s.countDigit(n);
 
-}  
+}
+
+//Time Complexity of above is Log n
+
+//Brute force:
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int count = 0;
+    int countDigit (int n) {
+        while ( n > 0) {
+        n = n/10;
+        count++; }
+        return count;
+    }
+};
+
+int main () {
+    Solution s;
+    int n; cin >> n;
+    cout << s.countDigit(n);
+
+}
+
+
+//Time complexity of above is O(n)
