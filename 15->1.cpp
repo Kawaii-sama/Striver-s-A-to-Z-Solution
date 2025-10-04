6474d4838746829732320c00d7c380ef3de9b22b
Serena <116498537+Kawaii-sama@users.noreply.github.com>
2025-10-04 22:41:01 +0530

    Update 15->1.cpp

--- 15->1.cpp
+++ 15->1.cpp
@@ -1,4 +1,4 @@
-//sum of natural numbers
+//sum of natural numbers , paramatrized-functional recursion
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -15,3 +15,48 @@ int main() {
     cout << sumNumbers(1,N);
     return 0;
 }
+
+/*
+we can also do this question by:
+1. normal for loop
+2. by n*(n-1) / 2 formula
+3. functional recursion
+4. paramatrized recursion
+*/
+
+// paramatrized recursion , print sum of first N integers
+#include <bits/stdc++.h>
+using namespace std;
+
+void printSum (int i, int sum) {
+    if (i < 1) {
+        cout << sum;
+        return;
+    }
+    printSum (i-1,sum+i);
+}
+
+int main () {
+    int n; cin >> n;
+    printSum(n,0);
+}
+
+
+
+
+
+// functional recursion , print sum of first N integers
+#include <bits/stdc++.h>
+using namespace std;
+
+int printSum (int i) {
+    if (i < 1) return 0;
+    return i + printSum (i-1);
+    
+}
+
+int main () {
+    int n; cin >> n;
+    cout << printSum(n);
+}
+
