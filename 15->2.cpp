1f648fbead950bdefed43c5ef070d94f2f82a3c2
Serena <116498537+Kawaii-sama@users.noreply.github.com>
2025-10-04 22:55:20 +0530

    Update 15->2.cpp

--- 15->2.cpp
+++ 15->2.cpp
@@ -1,4 +1,4 @@
-//factorial of natural numbers
+//factorial of natural numbers (paramatrzied + functional recursive)
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -15,3 +15,42 @@ int main() {
     cout << factNumbers(1,N);
     return 0;
 }
+
+
+// paramatrized recursion , factorial
+#include <bits/stdc++.h>
+using namespace std;
+
+int result = 1;
+void fact (int i, int n) {
+    if (i > n) {
+        cout << result;
+        return;
+    }
+    result *= i;
+    fact (i+1,n);
+    
+}
+
+int main () {
+    int n; cin >> n;
+    fact (1,n);
+}
+
+
+
+// functional recursion , factorial
+#include <bits/stdc++.h>
+using namespace std;
+
+int fact (int i) {
+    if (i < 1) return 1;
+    return i * fact (i-1);
+    
+}
+
+int main () {
+    int n; cin >> n;
+    cout << fact (n);
+}
+
