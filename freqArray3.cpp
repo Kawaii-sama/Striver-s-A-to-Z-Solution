0f1461cd9e56c0aec70dcfb9e4c6501b5b7202cb
Serena <116498537+Kawaii-sama@users.noreply.github.com>
2025-12-06 21:52:08 +0530

    Update frequency array comments and add new functionality

--- freqArray3.cpp
+++ freqArray3.cpp
@@ -1,4 +1,4 @@
-// frequency array of string. (precompute + fetch)
+// frequency array of string, lowercase only. (precompute + fetch)
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -15,6 +15,30 @@ int main() {
     while (n > 0) {
         cin >> ch2;
         cout << hash[ch2 - ch1] << " ";
+        n--;
+    }
+    return 0;
+    
+}
+
+
+// frequency array of string, all cases. (precompute + fetch)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s; cin >> s; char ch1 = 'a';
+    // precompute
+    int hash[256] = {0};
+    for (int i = 0; i < s.length(); i++) {
+        hash[s[i]] += 1;
+    }
+    
+    int n; cin >> n; char ch2;
+    // fetch
+    while (n > 0) {
+        cin >> ch2;
+        cout << hash[ch2] << " ";
         n--;
     }
     return 0;
