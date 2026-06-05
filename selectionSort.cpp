37b45ac6fd149a193c8c3c35b5719f1cb66212a2
Pratibha Aggarwal <pratibhaaggarwal32@gmail.com>
2026-06-05 14:28:21 +0530

    better?

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -5,19 +5,19 @@ using namespace std;
 class Solution {
 public:
     vector<int> selectionSort(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
+        // yeh i second last takk jayega, j last takk jayega
+        for (int i = 0; i < nums.size() - 1; i++) {
             int min = nums[i];
             for (int j = i + 1; j < nums.size(); j++) {
                 if ( nums[i] <= nums[j] ) continue;
                 else {
                     int temp = nums[j];
                     nums[j] = nums[i]; nums[i] = temp;
+                    // we can also switch min from j;
                 }
             }
         }
-
         return nums;
-
     }
 };
 
@@ -30,4 +30,6 @@ int main () {
     for (int i = 0; i < ans.size(); i++) {
         cout << ans[i] << " ";
     }
+    
+    // time complexity:- O(N^2) ; space complexity:- O(1);
 }
