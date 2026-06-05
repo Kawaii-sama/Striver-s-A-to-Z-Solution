4639943e17465520add20a1eb60ba1493695d76a
Pratibha Aggarwal <pratibhaaggarwal32@gmail.com>
2026-06-05 19:06:57 +0530

    better than striver's optimal

---
+++ myOptimalBubble.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+int swapped;
+    vector<int> bubbleSort(vector<int>& nums) {
+    do {
+    swapped = 0;
+       for (int i = 0; i < nums.size()-1; i++) {
+         if (nums[i] <= nums[i+1])  continue; 
+         else { int temp = nums[i+1];
+            nums[i+1] = nums[i]; nums[i] = temp; swapped = 1;}
+       }
+    } while (swapped == 1);
+        return nums;
+    
+    }
+
+
+};
+
+
+
+int main () {
+    Solution obj;
+    vector<int> ans = {7,5,3};
+    obj.bubbleSort(ans);
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
+    }
+}
+
